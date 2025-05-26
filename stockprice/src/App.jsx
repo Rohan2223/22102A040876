7a5635c491f2abc096a9c888baceaa4bec6460a6
Rohan2223 <kondururohan6789@gmail.com>
2025-05-26 11:11:03 +0530

    Update App.jsx

--- stockprice/src/App.jsx
+++ stockprice/src/App.jsx
@@ -2,11 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Stockprice from "./GoodList";
 import GoodList from './GoodList';
 function App() {
-  const [count, setCount] = useState(0)
-
  return (
     <div>
       <GoodList />
